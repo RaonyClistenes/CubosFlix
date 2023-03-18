e1abd8f2d90f3e2d8a4fd4413f7a966ede6acae9
Raony Clistenes <ynoar05@gmail.com>
2023-03-18 19:24:49 -0300

    Add files via upload

--- Cubos Flix/script.js
+++ Cubos Flix/script.js
@@ -1,239 +1,220 @@
 const filmes = document.querySelector('div .movies')
 const modal = document.querySelector('.modal')
-const modalTitle = document.querySelector('.modal__title')
+const modalTiulo = document.querySelector('.modal__title')
 const botaoFecharModal = document.querySelector('.modal__close')
-const modalImage = document.querySelector('.modal__img')
-const modalDescription = document.querySelector('.modal__description')
-const modal__average = document.querySelector('.modal__average');
+const modalImagem = document.querySelector('.modal__img')
+const modalDescricao = document.querySelector('.modal__description')
+const modal__media = document.querySelector('.modal__average');
 const containerFilmes = document.querySelector('div .container')
 const botaoDireita = document.querySelector('.btn-next');
 const botaoEsquerda = document.querySelector('.btn-prev');
-const temaPage = document.querySelector('.btn-theme')
+const temaPagina = document.querySelector('.btn-theme')
 const logo = document.querySelector('.header__container-logo img')
 const input = document.querySelector('input')
-
-
-
-async function movies() {
-
-    const idFilme = [];
-    const data = [];
-    const dataApi = [];
-    const nome = []
-    const cartaz = []
-    const rating = [];
-    const description = [];
-
-
-    const response = await api.get('/discover/movie?language=pt-BR&include_adult=false')
-    dataApi.push(response.data)
-    data.push(dataApi[0].results)
-
-    for (let i = 0; i < data.length; i++) {
-        for (let j = 0; j < data[i].length; j++) {
-            nome.push(data[i][j].title)
-            cartaz.push(data[i][j].poster_path)
-            rating.push(data[i][j].vote_average)
-            idFilme.push(data[i][j].backdrop_path)
-            description.push(data[i][j].overview)
-            console.log(cartaz)
-        }
-    }
-
-    for (let t = 0; t < nome.length; t++) {
-        const filme = document.createElement('div');
-        const info = document.createElement('div')
-        const movie_title = document.createElement('span')
-        const movie_rating = document.createElement('span')
-        const icone = document.createElement('img');
-
-        filmes.style.overflowX = 'auto'
-        filmes.style.overflow = 'hidden'
-        filme.style.flexShrink = 0
-        filmes.appendChild(filme)
-        filme.appendChild(info)
-        info.appendChild(movie_title)
-        info.appendChild(movie_rating)
-        filme.className = 'movie'
-        filme.style.backgroundImage = `url("${cartaz[t]}")`
-        info.className = 'movie__info'
-        movie_title.className = 'movie__title'
-        movie_rating.className = 'movie__rating'
-        icone.src = "/assets/estrela.svg"
-        movie_title.innerText = nome[t]
-        movie_rating.innerHTML = rating[t]
-        movie_rating.appendChild(icone)
-        
-        function busca() {
-            input.addEventListener('keydown', () => {
-                const provisorio = input.value
-                if (provisorio != nome[t]) {
-                    filme.classList.add('hidden')
-                }
-                if (!provisorio) {
-                    filme.classList.remove('hidden')
-                }
-                if(nome[t].includes(provisorio)){
-                    filme.classList.remove('hidden')
-                }
-                
-                
-            })
-        }
-        busca()
-
-
-        function modalInfo() {
-            modal.classList.toggle('hidden')
-
-            modalImage.src = idFilme[t]
-            modalTitle.innerHTML = nome[t]
-            modalDescription.innerHTML = description[t]
-            modal__average.innerHTML = rating[t]
-        }
-
-        botaoFecharModal.addEventListener('click', () => {
-
-            modal.classList.add("hidden")
-
-        })
-
-        let listFilmes = document.querySelectorAll('.movie')
-        if (listFilmes.length > 18) {
-            listFilmes[t].classList.add('hidden')
-        }
-        let indexArray = 0;
-        let maxIndex = listFilmes.length
-
-        function direita() {
-            botaoDireita.addEventListener('click', () => {
-                indexArray = indexArray + 6;
-                if (indexArray == maxIndex) {
-                    indexArray = 0
-                }
-                listFilmes[indexArray].scrollIntoView({
-                    inline: 'start',
-                    behavior: 'smooth'
-                })
-
-            })
-        }
-
-        function esquerda() {
-            botaoEsquerda.addEventListener('click', () => {
-                if (indexArray == 0) {
-                    indexArray = maxIndex
-                }
-                indexArray = indexArray - 6;
-
-                listFilmes[indexArray].scrollIntoView({
-                    inline: 'start',
-                    behavior: 'smooth'
-                })
-            })
-
-        }
-        esquerda()
-        direita()
-
-        function abrirModal() {
-            listFilmes[t].addEventListener('click', () => {
-                modalInfo()
-            })
-        }
-        abrirModal()
-
-    }
-
-
-
-}
-
-movies()
-
-const highlight__size = document.querySelector('div highlight')
-const video = document.querySelector('div .highlight__video')
-const textH1 = document.querySelector('div .highlight__title')
-const textRating = document.querySelector('div .highlight__rating')
-const gener = document.querySelector('div .highlight__genres')
-const launch = document.querySelector('div .highlight__launch')
-const description = document.querySelector('div .highlight__description')
-
-async function todayMovie() {
-    const response = await api.get('/movie/436969?language=pt-BR')
-    const dataApi = [];
-    const data = [];
-    const generos = [];
-    const generArray = [];
-    dataApi.push(response.data)
-    video.style.backgroundImage = `url("${dataApi[0].backdrop_path}")`
-    textH1.innerHTML = `${dataApi[0].title}`
-    textRating.innerHTML = dataApi[0].vote_average
-    for (let i = 0; i < dataApi.length; i++) {
-        data.push(dataApi[i].genres)
-        for (let j = 0; j < data[i].length; j++) {
-            generos.push(data[i][j])
-            generArray.push(generos[j].name)
-            let generString = generArray.join(', ')
-            gener.innerHTML = generString
-            description.innerHTML = dataApi[i].overview
-            launch.innerHTML = dataApi[i].release_date
-
-
-        }
-    }
-}
-
-todayMovie()
-
-
-const botaoplay = document.querySelector('div #play')
 const linkVideo = document.querySelector('div .highlight__video-link')
+const root = document.querySelector(':root')
+const filmeDoDiaImagem = document.querySelector('.highlight__video')
+const tituloFilmedoDia = document.querySelector('.highlight__title')
+const avaliacaoDoFilmeDoDia = document.querySelector('.highlight__rating')
+const generosFilmeDoDia = document.querySelector('.highlight__genres')
+const descricaoFilmeDoDia = document.querySelector('.highlight__description')
 
-
-async function playMovie() {
-
-    const response = await api.get('/movie/436969/videos?language=pt-BR');
-    const dataApi = []
-
-    dataApi.push(response.data)
-
-    botaoplay.addEventListener('click', () => {
-        linkVideo.href = 'https://www.youtube.com/watch?v=' + dataApi[0].results[0].key
-    })
-
+const apiPadrao = 'https://tmdb-proxy.cubos-academy.workers.dev/3/discover/movie?language=pt-BR&include_adult=false';
+const apiBusca = `https://tmdb-proxy.cubos-academy.workers.dev/3/search/movie?language=pt-BR&include_adult=false`;
+const apiVideoDoDia = "https://tmdb-proxy.cubos-academy.workers.dev/3/movie/436969/videos?language=pt-BR";
+const apiFilmeDoDia = "https://tmdb-proxy.cubos-academy.workers.dev/3/movie/436969?language=pt-BR";
+async function filmesPadrao() {
+  try {
+    const response = await fetch(apiPadrao);
+    const data = await response.json();
+    const resultados = data.results;
+    filmes.innerHTML = '';
+    for (let item = 0; item < 18; item++) {
+      const element = resultados[item];
+      const filme = document.createElement('div');
+      filmes.appendChild(filme);
+      filmes.style.overflowX = 'auto'
+      filmes.style.overflow = 'hidden'
+      filme.style.flexShrink = 0
+      filme.style.backgroundImage = `url("${element.poster_path}")`;
+      filme.className = 'movie';
+      const filmeInfo = document.createElement('div');
+      filmeInfo.className = 'movie__info'
+      filme.appendChild(filmeInfo);
+      const filmeTitulo = document.createElement('span');
+      filmeTitulo.className = 'movie__title'
+      filmeInfo.appendChild(filmeTitulo);
+      filmeTitulo.innerHTML = element.title;
+      const filmeavaliacao = document.createElement('span');
+      filmeavaliacao.className = 'movie__rating'
+      filmeInfo.appendChild(filmeavaliacao);
+      filmeavaliacao.innerHTML = element.vote_average;
+      const filmeEstrela = document.createElement('img');
+      filmeInfo.appendChild(filmeEstrela);
+      filmeEstrela.src = './assets/estrela.svg';
+      filme.addEventListener('click', () => {
+        modalPadrao(element)
+        abriModal()
+      })
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-playMovie()
+async function filmesBuscados(buscar) {
+  try {
+    const response = await fetch(`${apiBusca}&query=${buscar}`);
+    const data = await response.json();
+    const resultados = data.results;
+    filmes.innerHTML = '';
+    for (let item = 0; item < 18; item++) {
+      const element = resultados[item];
+      const filme = document.createElement('div');
+      filmes.appendChild(filme);
+      filmes.style.overflowX = 'auto'
+      filmes.style.overflow = 'hidden'
+      filme.style.flexShrink = 0
+      filme.style.backgroundImage = `url("${element.poster_path}")`;
+      filme.className = 'movie';
+      const filmeInfo = document.createElement('div');
+      filmeInfo.className = 'movie__info'
+      filme.appendChild(filmeInfo);
+      const filmeTitulo = document.createElement('span');
+      filmeTitulo.className = 'movie__title'
+      filmeInfo.appendChild(filmeTitulo);
+      filmeTitulo.innerHTML = element.title;
+      const filmeavaliacao = document.createElement('span');
+      filmeavaliacao.className = 'movie__rating'
+      filmeInfo.appendChild(filmeavaliacao);
+      filmeavaliacao.innerHTML = element.vote_average;
+      const filmeEstrela = document.createElement('img');
+      filmeInfo.appendChild(filmeEstrela);
+      filmeEstrela.src = './assets/estrela.svg';
+      filme.addEventListener('click', () => {
+        modalPadrao(element)
+        abriModal()
+      })
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+function modalPadrao(valor) {
+  modalImagem.src = valor.backdrop_path
+  modalTiulo.innerHTML = valor.title
+  modalDescricao.innerHTML = valor.overview
+  modal__media.innerHTML = valor.vote_average
+}
+async function abriModal() {
+  modal.classList.toggle('hidden')
+}
+function fecharModal() {
+  modal.classList.add('hidden')
+}
+botaoFecharModal.addEventListener('click', fecharModal)
 
+input.addEventListener('keyup', async () => {
+  const buscar = input.value.trim();
+  if (buscar !== '') {
+    await filmesBuscados(buscar);
+
+  } else {
+    await filmesPadrao();
+
+  }
+});
+
+
+let index = 0
+async function proximo() {
+  botaoDireita.addEventListener('click', async () => {
+    index += 6;
+
+    const arrayFilmes = document.querySelectorAll('.movie')
+    if (index >= arrayFilmes.length) {
+      index = 0
+    }
+    arrayFilmes[index].scrollIntoView({
+      inline: 'start',
+      behavior: 'smooth'
+    })
+
+  })
+}
+proximo()
+async function anterior() {
+  botaoEsquerda.addEventListener('click', async () => {
+    index -= 6;
+    const arrayFilmes = document.querySelectorAll('.movie')
+    if (index < 0) {
+      index = arrayFilmes.length
+    }
+    arrayFilmes[index].scrollIntoView({
+      inline: 'start',
+      behavior: 'smooth'
+    })
+
+  })
+}
+anterior()
+async function filmeDoDia() {
+  let generos = [];
+  try {
+    const response = await fetch(apiFilmeDoDia)
+    const data = await response.json()
+    const resultados = data
+    resultados.genres.forEach(item => generos.push(item))
+    filmeDoDiaImagem.style.backgroundImage = `url("${resultados.backdrop_path}")`;
+    tituloFilmedoDia.innerHTML = resultados.title
+    avaliacaoDoFilmeDoDia.innerHTML = resultados.vote_average
+    generos.forEach(item => generosFilmeDoDia.innerHTML += item.name + ' ')
+    descricaoFilmeDoDia.innerHTML = resultados.overview
+  } catch (error) {
+    console.log(error)
+  }
+}
+async function play() {
+  try {
+    const response = await fetch(apiVideoDoDia)
+    const data = await response.json()
+    const resultados = data.results
+    for (let item = 0; item < resultados.length; item++) {
+      const element = resultados[item];
+      linkVideo.href = `https://www.youtube.com/watch?v=${element.key}`
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+window.addEventListener('load', async () => {
+  await filmesPadrao();
+  await filmeDoDia();
+  await play()
+
+});
 function tema() {
+  const root = document.querySelector(':root')
+  const back = root.style.getPropertyValue('--background');
+  if (!back || back === 'white') {
+    root.style.setProperty('--background', '#1B2028')
+    root.style.setProperty('--text-color', 'white')
+    root.style.setProperty('--bg-secondary', '#2D3440')
+    temaPagina.src = "./assets/dark-mode.svg"
+    logo.src = "./assets/logo.svg"
+    botaoDireita.src = '/assets/arrow-right-light.svg'
+    botaoEsquerda.src = '/assets/arrow-left-light.svg'
+    botaoFecharModal.src = './assets/close.svg'
+  } else {
+    root.style.setProperty('--background', 'white')
+    root.style.setProperty('--text-color', 'black')
+    root.style.setProperty('--bg-secondary', '#EDEDED')
+    temaPagina.src = "./assets/light-mode.svg"
+    logo.src = "./assets/logo-dark.png"
+    botaoDireita.src = './assets/arrow-right-dark.svg'
+    botaoEsquerda.src = './assets/arrow-left-dark.svg'
+    botaoFecharModal.src = './assets/close-dark.svg'
 
-    temaPage.addEventListener('click', () => {
-
-        const root = document.querySelector(':root')
-        const back = root.style.getPropertyValue('--background');
-        const back2 = root.style.getPropertyValue('--bg-secondary');
-        if (!back || back === 'white') {
-            root.style.setProperty('--background', '#1B2028')
-            root.style.setProperty('--text-color', 'white')
-            root.style.setProperty('--bg-secondary', '#2D3440')
-            temaPage.src = "./assets/dark-mode.svg"
-            logo.src = "./assets/logo.svg"
-            botaoDireita.src = '/assets/arrow-right-light.svg'
-            botaoEsquerda.src = '/assets/arrow-left-light.svg'
-            botaoFecharModal.src = './assets/close.svg'
-        } else {
-            root.style.setProperty('--background', 'white')
-            root.style.setProperty('--text-color', 'black')
-            root.style.setProperty('--bg-secondary', '#EDEDED')
-            temaPage.src = "./assets/light-mode.svg"
-            logo.src = "./assets/logo-dark.png"
-            botaoDireita.src = './assets/arrow-right-dark.svg'
-            botaoEsquerda.src = './assets/arrow-left-dark.svg'
-            botaoFecharModal.src = './assets/close-dark.svg'
-
-        }
-    })
+  }
 }
-tema()
-
+temaPagina.addEventListener('click', tema)
